e8026297b81b473d44ea9db853c38c840ca4e0aa
zineb elkhallai <zinebelkhalladi.2019@gmail.com>
2020-02-07 18:47:01 +0100

    t

---
+++ audio/image/script.js
@@ -0,0 +1,85 @@
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.curser = null;
+    }
+
+    add(data) {
+        if (!this.head) {
+            this.head = new Node(data);
+            this.curser = this.head;
+        } else {
+            let temp = this.head;
+            this.head = new Node(data);
+            temp.next = this.head;
+            this.head.prev = temp;
+        }
+    }
+    pop() {
+        if(this.head === this.curser){
+            this.curser = this.curser.prev;
+        }
+        if (this.head && this.head.prev) {
+            this.head = this.head.prev;
+            this.head.next = null;
+        }
+    }
+    showCurrent() {
+        if (this.curser) {
+            return this.curser.data;
+        } else {
+            return "https://placehold.it/800x500";
+        }
+    }
+    next() {
+        if (this.curser && this.curser.next) {
+            this.curser = this.curser.next;
+        }
+    }
+    prev() {
+        if (this.curser && this.curser.prev) {
+            this.curser = this.curser.prev;
+        }
+    }
+}
+class Node {
+    constructor(data) {
+        this.next = null;
+        this.prev = null;
+        this.data = data;
+    }
+}
+
+let list = new LinkedList();
+
+function add() {
+    list.add(document.getElementById("data").value);
+    document.getElementById("img").src = list.showCurrent();
+    document.getElementById("data").value = "";
+
+}
+
+function pop() {
+    list.pop();
+    document.getElementById("img").src = list.showCurrent();
+}
+
+function next() {
+    list.next();
+    document.getElementById("img").src = list.showCurrent();
+}
+
+function prev() {
+    list.prev();
+    document.getElementById("img").src = list.showCurrent();
+}
+
+//menu responsive
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
